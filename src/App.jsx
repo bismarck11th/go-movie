f90e490ca18d8a3d2c0e5f910bf55607aa95aeb6
kazuki827 <xm.kazuki827@gmail.com>
2022-01-11 23:37:52 +0900

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <h1 className="mt-3">Go Watch a Movie!</h1>
+        <hr className="mb-3" />
+      </div>
+
+      <div className="row">
+        <div className="col-md-2"></div>
+        <div className="col-md-10"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
